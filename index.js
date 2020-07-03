3eac1fa53ebe739fb605fc5da7fb4d86748d1c8e
muellerluke <muellerluke24@gmail.com>
2020-07-02 19:27:18 -0500

    do not require token for /movies endpoint

--- index.js
+++ index.js
@@ -78,20 +78,16 @@ app.get(
   }
 );
 
-app.get(
-  "/movies",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Movies.find()
-      .then((movies) => {
-        res.status(201).json(movies);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error: " + err);
-      });
-  }
-);
+app.get("/movies", (req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
 app.get(
   "/genres/:Genre",
