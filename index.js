0b875fff5c39d52ef34b014dcb14fd9d8ab987df
muellerluke <muellerluke24@gmail.com>
2020-07-29 12:17:31 -0500

    enable jwt authentication for movies endpoint

--- index.js
+++ index.js
@@ -83,16 +83,20 @@ app.get(
   }
 );
 
-app.get("/movies", (req, res) => {
-  Movies.find()
-    .then((movies) => {
-      res.status(201).json(movies);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/movies",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Movies.find()
+      .then((movies) => {
+        res.status(201).json(movies);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 app.get(
   "/genres/:Genre",
