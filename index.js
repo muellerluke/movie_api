3450bd07a939678f18ecf5fa5f92ea12a9d6a58f
muellerluke <muellerluke24@gmail.com>
2020-07-29 12:24:22 -0500

    cors policy update

--- index.js
+++ index.js
@@ -32,22 +32,17 @@ app.use((err, req, res, next) => {
   res.status(500).send("Sumthin broked");
 });
 
-let allowedOrigins = [
-  "http://localhost:8080",
-  "http://testsite.com",
-  "*",
-  "http://localhost:1234",
-];
+let allowedOrigins = ["*"];
 app.use(
   cors({
     origin: (origin, callback) => {
       if (!origin) return callback(null, true);
-      //if (allowedOrigins.indexOf(origin) === -1) {
-      //let message =
-      // "The CORS Policy for this application does not allow access from origin " +
-      //  origin;
-      // return callback(new Error(message), false);
-      // }
+      if (allowedOrigins.indexOf(origin) === -1) {
+        let message =
+          "The CORS Policy for this application does not allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
       return callback(null, true);
     },
   })
